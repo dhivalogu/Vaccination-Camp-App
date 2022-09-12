02201a50a9b375c2c14626ad14458509e3c1fbe4
Dhiva <dhivalogu@gmail.com>
2022-09-12 13:58:38 +0530

    add print functionality to booking data

---
+++ app/components/view-booking.js
@@ -0,0 +1,17 @@
+import Ember from "ember";
+
+export default Ember.Component.extend({
+  actions: {
+    closeModal() {
+      document
+        .getElementById("registration-modal-container")
+        .classList.add("hidden");
+      document.getElementById("registration-modal-container").style.display =
+        "none";
+    },
+    print() {
+      document.title = "Certificate of Vaccination";
+      window.print();
+    },
+  },
+});
--- app/controllers/user/profile.js
+++ app/controllers/user/profile.js
@@ -8,5 +8,13 @@ export default Ember.Controller.extend({
       localStorage.removeItem("user_id");
       this.transitionToRoute("accounts");
     },
+    viewBooking(booking) {
+      this.set("choosenBooking", booking);
+      document
+        .getElementById("registration-modal-container")
+        .classList.remove("hidden");
+      document.getElementById("registration-modal-container").style.display =
+        "flex";
+    },
   },
 });
